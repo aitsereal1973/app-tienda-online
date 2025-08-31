72751d987930b8a45e31305756fc022ab59d65c2
aitsereal1973 <odidoquinom@hotmail.com>
2025-08-31 10:02:56 -0500

    feat: initial commit for src/App.js

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+const App = () => {
+  return (
+    <View>
+      <Text>Bienvenido a App Tienda Online!</Text>
+    </View>
+  );
+};
+
+export default App;
